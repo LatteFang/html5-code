53a484baa438061a7a6a1b935c9e3edcea09e519
fangjiaqi77 <370358679@qq.com>
2020-03-01 22:33:36 +0800

    update

--- full-stack/server.py
+++ full-stack/server.py
@@ -1,12 +1,12 @@
 from http.server import HTTPServer, BaseHTTPRequuestHandler
 
 class Handler(BaseHTTPRequuestHandler):
-    def do_sth_for_GET(self):
+    def do_GET(self):
         self.send_response(200)
         self.send_header('Content_type','text/plain;charset=utf-8')
         self.end_header()
 
-        self.wfile.write("Hello everyone, I am AI server~\n".encode())
+        self.wfile.write("Hello everyone, I am AI server~ And this is a page of:".encode()+self.path[1:].encode())
 
 if __name__ =='__main__':
     server_address=('',8000)
